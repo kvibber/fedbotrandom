3522b93bc4a720eeccb717365fdc3c55d6560262
Kelson Vibber <kelson@pobox.com>
2018-09-15 22:06:32 -0700

    Initial script and readme

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # fedbotrandom
 Simple bot to post a random line from a text file to Mastodon.
+By Kelson Vibber. https://github.com/kvibber/fedbotrandom
+
+Give it an access token and a text file with items to post, and it will pick a random line from the file and post that line to your Mastodon account.
+
+For now, configuration is done in the script itself.
+Add the hostname of your instance (ex. botsin.space) and the access token,
+which you can get by creating an app in your Mastodon preferences at
+Preferences/Development/Your Applications
+For example: https://botsin.space/settings/applications
+
+Call the script to post one random line from the text file:
+     fedbotrandom.pl quotes.txt
+     
+To run it regularly, you can schedule it as a cron job.
+
+
+TODO:
+I'm deliberately not making this complicated, but I will probably add:
+- multiline posts.
+- external config file.
---
+++ fedbotrandom.pl
@@ -0,0 +1,48 @@
+#!/bin/perl
+# Super-simple bot that picks a random line from a text file and posts it to
+# an account on Mastodon.
+#
+# Configure by adding your instance hostname and access token here.
+# Call with the text filename as the parameter
+#      fedbotrandom.pl quotes.txt
+#
+# To run regularly, just schedule it as a cron job.
+# By Kelson Vibber, https://github.com/kvibber/fedbotrandom
+
+use strict;
+use warnings;
+use LWP;
+  
+# Configuration. Get your access token by creating an app in your Mastodon
+# preferences at Preferences/Development/Your Applications
+# For example, https://botsin.space/settings/applications
+my $INSTANCE_HOST='botsin.space';
+my $API_ACCESS_TOKEN='your_access_token';
+
+# Read the list from the file given on the command line.
+my $listSource = $ARGV[0];
+open listFile, $listSource;
+my @list = <listFile>;
+close listFile;
+
+# Randomly select an element from the list
+my $content = "";
+my $loopsRemaining = 10;
+while($content eq "" && $loopsRemaining > 0) {
+	my $index = int(rand(scalar @list));
+	$content = $list[$index];
+	$content =~ s/^\s+|\s+$//g;
+	$loopsRemaining--;
+}
+
+my $url = "https://$INSTANCE_HOST/api/v1/statuses?access_token=$API_ACCESS_TOKEN";
+
+print "Going to post $content to $url";
+
+my $browser = LWP::UserAgent->new;
+my $response = $browser->post( $url,
+   [
+     status => $content, 
+     visibility => 'unlisted'
+   ],
+);
